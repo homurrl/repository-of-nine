021ecdfb6bf0e4eb0982baf2dba854b9270a8335
Brea Grindley <breagrindley@Breas-MacBook-Pro.local>
2023-05-26 14:05:13 +0930

    added generate markdown functions

--- index.js
+++ index.js
@@ -6,89 +6,89 @@ const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
 const questions = [
     {
-      type: "input",
-      message: "Enter your github username",
-      name: "Github",
-      validate: validateInput,
+        type: "input",
+        message: "Enter your github username",
+        name: "Github",
+        validate: validateInput,
     },
     {
-      type: "input",
-      message: "Enter your email",
-      name: "email",
-      validate: validateInput,
+        type: "input",
+        message: "Enter your email",
+        name: "email",
+        validate: validateInput,
     },
     {
-      type: "input",
-      message: "Enter the title of your project",
-      name: "title",
-      validate: validateInput,
+        type: "input",
+        message: "Enter the title of your project",
+        name: "title",
+        validate: validateInput,
     },
     {
-      type: "input",
-      message: "Describe your project",
-      name: "description",
-      validate: validateInput,
+        type: "input",
+        message: "Describe your project",
+        name: "description",
+        validate: validateInput,
     },
     {
-      type: "list",
-      message: "Select your licenses",
-      choices: ["None", "MIT", "Apache", "MPL", "GPL", "AGPL"],
-      name: "license",
+        type: "list",
+        message: "Select your licenses",
+        choices: ["None", "MIT", "Apache", "MPL", "GPL", "AGPL"],
+        name: "license",
     },
     {
-      type: "input",
-      message: "How can someone use your project?",
-      name: "usage",
-      validate: validateInput,
+        type: "input",
+        message: "How can someone use your project?",
+        name: "usage",
+        validate: validateInput,
     },
     {
-      type: "input",
-      message: "Add a screenshot?",
-      name: "screenshot",
+        type: "input",
+        message: "Include a screenshot",
+        name: "screenshot",
     },
     {
-      type: "input",
-      message: "What commands should be run to install dependencies?",
-      name: "install",
-      validate: validateInput,
+        type: "input",
+        message: "Include command to run dependancies",
+        name: "install",
+        validate: validateInput,
     },
     {
-      type: "input",
-      message: "What commands should be run to run tests?",
-      name: "tests",
-      validate: validateInput,
+        type: "input",
+        message: "What command runs tests?",
+        name: "tests",
+        validate: validateInput,
     },
     {
-      type: "input",
-      message: "What does the user need to know about contributing to this repo?",
-      name: "contribute",
-      validate: validateInput,
+        type: "input",
+        message: "What does the user need to know about contributing to this repo?",
+        name: "contribute",
+        validate: validateInput,
     },
-  ];
+];
 
 //inquirer questions function
 
 function inquire() {
     return inquirer.prompt(questions);
-  }
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (error) => {
-      error ? console.log("Error. Information not logged: " + error) : console.log("README generated");
+        error ? console.log("Error. Information not logged: " + error) : console.log("README generated");
     });
-  }
+}
 
-  //generates and stores README file in the README directory
+//generates and stores README file in the README directory
 
-  async function init() {
+async function init() {
     const responses = await inquire();
     const markdown = generateMarkdown(responses);
     writeToFile("./README", markdown);
-  }
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,58 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'Apache') {
+    return '![License](https://img.shields.io/badge/license-Apache-blue)'
+  } else if (license === 'MIT') {
+    return '![License](https://img.shields.io/badge/license-MIT-informational)'
+  } else if (license === 'GPL') {
+    return '![License](https://img.shields.io/badge/license-GPL-blue)'
+  } else {
+    return ''
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'Apache') {
+    return 'https://opensource.org/licenses/Apache-2.0/'
+  } else if (license === 'MIT') {
+    return 'https://opensource.org/licenses/MIT/'
+  } else if (license === 'GPL') {
+    return 'https://www.gnu.org/licenses/gpl-3.0.en.html/'
+  } else {
+    return ''
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+${renderLicenseBadge(data.license[0])}
+## Table of Contents
+- [Description](#describe)
+- [Installation](#install)
+- [Usage](#use)
+- [Contribution](#contributions)
+- [License](#license)
+- [Github](#github)
+## Description:
+ ${data.describe}
+## Installation:
+${data.install}
+## Usage:
+${data.use}
+## Contribution:
+${data.contributions}
+## License:
+[${data.license[0]}](${renderLicenseLink(data.license[0])})
+## GitHub:
+[${data.github}](https://github.com/${data.github})
 `;
 }
 
